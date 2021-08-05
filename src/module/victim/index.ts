0d9bc087497098fa917e5d97b7ae693ec05f4aa2
joshualiangxy <joshualiang.xy@gmail.com>
2021-08-05 23:04:00 +0800

    Remove comments

--- src/module/victim/index.ts
+++ src/module/victim/index.ts
@@ -94,53 +94,5 @@ export default class Victim {
   public toString(): string {
     return this.accountId;
   }
-
-  /*
-  private currentGameId: number;
-  private currentGameName: string;
-  private prevGameId: number;
-  private prevGameName: string;
-  private lastSeenIngame: DateTime;
-
-  constructor(accountId: string) {
-    this.accountId = accountId;
-    this.lastSeenIngame = DateTime.local();
-  }
-
-  isSameVictim(accountId: string): boolean {
-    return this.accountId === accountId;
-  }
-
-  isPlayingNewGame(gameId: number, gameName: string): boolean {
-    if (gameId == NaN || gameName == null) return false;
-
-    if (this.prevGameId == NaN || this.prevGameName == null
-       || (this.prevGameName == gameName
-           && this.prevGameId == this.prevGameId)) {
-      return DateTime.local().diff(this.lastSeenIngame).as('milliseconds')
-          > Victim.timeLimit;
-    }
-
-    return true;
-  }
-
-  isStillIngame(gameId: number, gameName: string): boolean {
-    return this.currentGameId == gameId && this.currentGameName == gameName;
-  }
-
-  resetLastSeenTime(): void {
-    this.lastSeenIngame = DateTime.local();
-  }
-
-  setPrevGame(gameId: number, gameName: string): void {
-    this.prevGameId = gameId;
-    this.prevGameName = gameName;
-  }
-
-  setCurrGame(gameId: number = this.currentGameId, gameName: string): void {
-    this.currentGameId = gameId;
-    this.currentGameName = gameName;
-  }
-  */
 }
 
